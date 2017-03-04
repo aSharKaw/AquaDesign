23bf7f60f0065f37e5e1954eacf1502bb44a69d4
Shu <syonyan06@gmail.com>
2017-03-05 07:36:45 +0900

    UIの生成スクリプト追加

--- Assets/Script/BiologicalManager.cs
+++ Assets/Script/BiologicalManager.cs
@@ -4,69 +4,34 @@ using UnityEngine;
 
 public class BiologicalManager : MonoBehaviour {
 
-    //外部ファイル読み込むまでの代理参照
-    [SerializeField]
-    private GameObject NeonTetra;
-    [SerializeField]
-    private GameObject Leaf;
-    [SerializeField]
-    private GameObject Anubias;
-    [SerializeField]
-    private GameObject TigerOscar;
+    //テスト
+    //[System.NonSerialized]
+    private string _fish_name;
+
+    private int _fish_count;
 
     private int _neonTetra_count;
     private int _tigerOscar_count;
 
 	void Start ()
     {
-	    if(NeonTetra == null)
-        {
-            NeonTetra = Resources.Load("Prefub/NeonTetra") as GameObject;
-        }
-        if (Leaf == null)
-        {
-            Leaf = Resources.Load("Prefub/Leaf") as GameObject;
-        }
-        if (Anubias == null)
-        {
-            Anubias = Resources.Load("Prefub/Anubias") as GameObject;
-        }
-        if (TigerOscar == null)
-        {
-            TigerOscar = Resources.Load("Prefub/TigerOscar") as GameObject;
-        }
 
         _neonTetra_count = 0;
         _tigerOscar_count = 0;
     }
 	
-    public void NeonTetraCreate()
+    public void FishCreate(string fish_name)
     {
-        GameObject _fish;
-        _fish = Instantiate(NeonTetra);
-        _fish.name = "NeonTetra" + _neonTetra_count;
-        _neonTetra_count++;
+        GameObject _fish = Resources.Load("Prefub/" + fish_name) as GameObject;
+        _fish = Instantiate(_fish);
+        _fish.name = fish_name + _fish_count;
+        _fish_count++;
     }
 
     public void NeonTetraDelete()
     {
         _neonTetra_count--;
         GameObject _fish = GameObject.Find("NeonTetra" + _neonTetra_count);
-        Destroy(_fish);
-    }
-
-    public void TigerOscarCreate()
-    {
-        GameObject _fish;
-        _fish = Instantiate(TigerOscar);
-        _fish.name = "TigerOscar" + _tigerOscar_count;
-        _tigerOscar_count++;
-    }
-
-    public void TigerOscarDelete()
-    {
-        _tigerOscar_count--;
-        GameObject _fish = GameObject.Find("TigerOscar" + _neonTetra_count);
         Destroy(_fish);
     }
 
