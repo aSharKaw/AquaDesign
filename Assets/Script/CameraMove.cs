d66a7ac6635ee8ace18306b3a2c8221f04d1ba6f
Shu <syonyan06@gmail.com>
2017-03-17 00:50:55 +0900

    カメラ移動範囲を調整

--- Assets/Script/CameraMove.cs
+++ Assets/Script/CameraMove.cs
@@ -3,40 +3,84 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //カメラ移動用スクリプト
-public class CameraMove : MonoBehaviour {
+public class CameraMove : MonoBehaviour
+{
+    private Vector3 DEFAULT_POS = new Vector3(0, 0, 0.7f);
 
-    private int side_count;
-    private int length_count;
+    //移動速度
+    private float _move_speed;
+
+    //ボタン長押し判定
+    private bool up_button;
+    private bool down_button;
+    private bool left_button;
+    private bool right_button;
 
     void Start()
     {
-        side_count = 0;
-        length_count = 0;
+        _move_speed = 0.5f * Time.deltaTime;
     }
 
-	void Update ()
+    public void UpButtonPush()
     {
-		if(Input.GetKey(KeyCode.LeftArrow) && side_count < 0.5f * 60)
-        {
-            gameObject.transform.RotateAround(Vector3.zero, Vector3.up, 1);
-            side_count++;
-        }
-        if (Input.GetKey(KeyCode.RightArrow) && side_count > -0.5f * 60)
-        {
-            gameObject.transform.RotateAround(Vector3.zero, Vector3.down, 1);
-            side_count--;
-        }
+        up_button = true;
+    }
+    public void UpButtonRelease()
+    {
+        up_button = false;
+    }
 
-        if (Input.GetKey(KeyCode.UpArrow) && length_count < 0.5f * 60)
-        {
-            gameObject.transform.RotateAround(Vector3.zero, Vector3.left, 20 * Time.deltaTime);
-            length_count++;
-        }
-        if (Input.GetKey(KeyCode.DownArrow) && length_count > -0.0f * 60)
-        {
-            gameObject.transform.RotateAround(Vector3.zero, Vector3.right, 20 * Time.deltaTime);
-            length_count--;
-        }
+    public void DownButtonPush()
+    {
+        down_button = true;
+    }
+    public void DownButtonRelease()
+    {
+        down_button = false;
+    }
 
+    public void LeftButtonPush()
+    {
+        left_button = true;
+    }
+    public void LeftButtonRelease()
+    {
+        left_button = false;
+    }
+
+    public void RightButtonPush()
+    {
+        right_button = true;
+    }
+    public void RightButtonRelease()
+    {
+        right_button = false;
+    }
+
+    public void ResetButton()
+    {
+        gameObject.transform.position = DEFAULT_POS;
+        gameObject.transform.LookAt(Vector3.zero);
+    }
+
+    private void Update()
+    {
+        if(up_button)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, new Vector3(transform.position.x, 0.2f, 0.6f), _move_speed);
+        }
+        else if (down_button)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, new Vector3(transform.position.x, 0, 0.7f), _move_speed);
+        }
+        else if(left_button)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, new Vector3(0.35f, transform.position.y, 0.6f), _move_speed);
+        }
+        else if(right_button)
+        {
+            gameObject.transform.position = Vector3.MoveTowards(gameObject.transform.position, new Vector3(-0.35f, transform.position.y, 0.6f), _move_speed);
+        }
+        gameObject.transform.LookAt(Vector3.zero);
     }
 }
