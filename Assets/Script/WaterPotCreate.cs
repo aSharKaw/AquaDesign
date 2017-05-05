d5c919b73117928cf4384d2244060ed9bca2b710
Shu <syonyan06@gmail.com>
2017-05-05 19:22:23 +0900

    水槽を切り替えられるようにした

---
+++ Assets/Script/WaterPotCreate.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaterPotCreate : MonoBehaviour {
+
+    [SerializeField]
+    GameObject _waterpot60;
+    [SerializeField]
+    GameObject _waterpotcube;
+    [SerializeField]
+    GameObject _waterpotsphere;
+    [SerializeField]
+    GameObject _waterpothalfsphere;
+
+    void Start () {
+    }
+
+	void Update () {
+	}
+
+    public void CreatePot60()
+    {
+        GameObject waterpot = GameObject.Find("WaterPot");
+        Destroy(waterpot);
+
+        GameObject createPot = Instantiate(_waterpot60);
+        createPot.name = "WaterPot";
+    }
+
+    public void CreatePotCube()
+    {
+        GameObject waterpot = GameObject.Find("WaterPot");
+        Destroy(waterpot);
+
+        GameObject createPot = Instantiate(_waterpotcube);
+        createPot.name = "WaterPot";
+    }
+
+    public void CreatePotSphere()
+    {
+        GameObject waterpot = GameObject.Find("WaterPot");
+        Destroy(waterpot);
+
+        GameObject createPot = Instantiate(_waterpotsphere);
+        createPot.name = "WaterPot";
+    }
+
+    public void CreatePotHalfSphere()
+    {
+        GameObject waterpot = GameObject.Find("WaterPot");
+        Destroy(waterpot);
+
+        GameObject createPot = Instantiate(_waterpothalfsphere);
+        createPot.name = "WaterPot";
+    }
+}
