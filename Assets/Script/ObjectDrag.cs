7642b9abbb3e63f5e53f90a4b30221777c627753
Atsushi Yamashita <Atsushi Yamashita>
2017-09-14 11:32:19 +0900

    Update script

--- Assets/Script/ObjectDrag.cs
+++ Assets/Script/ObjectDrag.cs
@@ -1,47 +1,43 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ObjectDrag : MonoBehaviour {
+public class ObjectDrag : MonoBehaviour
+{
     [SerializeField]
     private Camera _camera;
 
     [SerializeField]
     private LayerMask _mask;
 
-    void Start()
+    private Vector3 GetRayHitPoint ( )
+    {
+        RaycastHit hit;
+        Ray ray = _camera.ScreenPointToRay( Input.mousePosition );
+
+        if (Physics.Raycast( ray, out hit, 1.0f, _mask ))
+        {
+            return hit.point;
+        }
+        return new Vector3( 0, 0, -100 );
+    }
+
+    private void OnMouseDrag ( )
+    {
+        this.transform.position = GetRayHitPoint(/*_ray*/);
+    }
+
+    private void OnMouseUp ( )
+    {
+        if (gameObject.transform.position == new Vector3( 0, 0, -100 ))
+        {
+            Destroy( gameObject );
+        }
+    }
+
+    private void Start ( )
     {
         if (this._camera == null)
         {
             _camera = Camera.main;
         }
     }
-
-    Vector3 GetRay(/*Ray ray*/)
-    {
-        RaycastHit hit;
-        Ray ray = _camera.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(ray, out hit, 1.0f, _mask))
-        {
-            return hit.point;
-        }
-        return new Vector3(0, 0, -100);
-    }
-
-    private void OnMouseDrag()
-    {
-        this.transform.position = GetRay(/*_ray*/);
-    }
-
-
-
-    private void OnMouseUp()
-    {
-        if (gameObject.transform.position == new Vector3(0, 0, -100))
-        {
-            Destroy(gameObject);
-        }
-    }
-
 }
